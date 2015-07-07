767979e8271a6ddb92f3eb56d91cf3888465e430
Brandon Braun <brandon.braun@gmail.com>
2015-07-06 22:45:13 -0400

    Completed section 11, about 51 percent completed

--- crowdfund/app/controllers/projects_controller.rb
+++ crowdfund/app/controllers/projects_controller.rb
@@ -6,4 +6,14 @@ class ProjectsController < ApplicationController
 	def show
 		@project = Project.find(params[:id])
 	end
+
+	def edit
+		@project = Project.find(params[:id])
+	end
+
+	def update
+		@project = Project.find(params[:id])
+		@project.update(params.require(:project).permit(:name, :description, :website, :target_pledge_amount, :pledging_ends_on))
+		redirect_to @project
+	end
 end
