73185fa45a25954494d08fe8f8375ea816d473a8
Brandon Braun <brandon.braun@gmail.com>
2015-07-09 18:57:13 -0400

    Completed section 16, 65 percent

--- crowdfund/app/helpers/projects_helper.rb
+++ crowdfund/app/helpers/projects_helper.rb
@@ -6,4 +6,8 @@ module ProjectsHelper
 			distance_of_time_in_words_to_now(project.pledging_ends_on) + " remaining"
 		end
 	end
+
+	def image_for(project)
+		image_tag(project.image_file_name.blank? ? "placeholder.png" : project.image_file_name)
+	end
 end
