a681ecdc846e4f78eef49ae06e3b9b1044492dbf
Brandon Braun <brandon.braun@gmail.com>
2015-07-09 22:14:02 -0400

    First half of section 19, 77 percent completed

--- crowdfund/app/models/project.rb
+++ crowdfund/app/models/project.rb
@@ -1,4 +1,16 @@
 class Project < ActiveRecord::Base
+	validates :name, :description, presence: true
+	validates :description, length: { maximum: 500 }
+	validates :target_pledge_amount, numericality: { greater_than: 0 }
+	validates :website, allow_blank: true, format: {
+		with:		 /https?:\/\/[\S]+\.[\S]+/i,
+		message: "must be a valide URL"
+	}
+	validates :image_file_name, allow_blank: true, format: {
+		with:		 /\w+\.(gif|jpg|png)\z/i,
+		message: "must reference a GIF, JPG, or PNG image"
+	}
+
   def self.accepting_pledges
     where("pledging_ends_on >= ?", Time.now).order("pledging_ends_on asc")
   end
