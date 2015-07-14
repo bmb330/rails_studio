ed6b49d84bb95e6350a5f5d8e3a7ae6a888e5a9c
Brandon Braun <brandon.braun@gmail.com>
2015-07-13 21:46:15 -0400

    Completed section 23, 92 percent

--- crowdfund/app/controllers/pledges_controller.rb
+++ crowdfund/app/controllers/pledges_controller.rb
@@ -1,6 +1,16 @@
 class PledgesController < ApplicationController
+	before_action :set_project
 	def index
-		@project = Project.find(params[:project_id])
 		@pledges = @project.pledges
+	end
+
+	def new
+		@pledge = @project.pledges.new
+	end
+
+	private
+	
+	def set_project
+		@project = Project.find(params[:project_id])
 	end
 end
